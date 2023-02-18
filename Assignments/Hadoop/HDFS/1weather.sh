e2a1e1a312bc9897ba825027249f014c9737a51b
shashankraj <shashanksara03@gmail.com>
2023-02-18 09:55:04 +0530

    Assignments

--- Assignments/Hadoop/HDFS/1weather.sh
+++ Assignments/Hadoop/HDFS/1weather.sh
@@ -1,14 +1,14 @@
-# Assignment #1:
-# 	Create directory "weather" under /user/training
-# 	Load the data from ~/futurence_hadoop-pyspark/labs/dataset/weather to /user/training/weather
-# 	Display the weather data
-# 	Split the weather data file and store as weather1 and wearther2
-# 	Merge the weather data file as weather3
-# 	Display the weather stats
+hdfs dfs -put /mnt/c/Users/miles.MILE-BL-4766-LA.000/Documents/FT/futurense_hadoop-pyspark/labs/dataset/weather/weather_data.txt  /user/training/weather
+hdfs dfs -cat /user/training/weather/weather_data.txt
 
-hadoop fs -mkdir /user/training/weather
+lines=`wc -l /user/training/weather/weather_data.txt`
 
-hadoop fs -put ~/futurence_hadoop-pyspark/labs/dataset/weather /user/training/weather
+line=`echo $lines | cut -d' ' -f1`
 
-hadoop fs -cat /user/training/weather/weather_data.txt
+h1=`expr $line / 2`
+h2=`expr $line - $h1`
 
+hdfs dfs -cat /user/training/weather/weather_data.txt | head -$h1 > weather1.txt
+hdfs dfs -cat /user/training/weather/weather_data.txt | tail -$h2 > weather2.txt
+
+cat weather1.txt weather2.txt > weather3.txt
